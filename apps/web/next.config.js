68741b6a6ca929a659778ac035c9f8a43bdac9a8
jaime assis <juniorassis1415@gmail.com>
2026-04-27 16:16:44 -0300

    perf: remotePatterns para next/image + nenhuma tag img encontrada para substituir

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,13 @@ const { withSentryConfig } = require('@sentry/nextjs')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@consorte/types'],
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: 'pub-*.r2.dev' },
+    ],
+  },
 }
 
 module.exports = withSentryConfig(nextConfig, {
