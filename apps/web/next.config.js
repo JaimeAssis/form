421575a6545fb80e6b3f4df79d8f3573bd40ddda
jaime assis <juniorassis1415@gmail.com>
2026-04-27 11:30:18 -0300

    feat: integrar Sentry no frontend (apps/web)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,13 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@consorte/types'],
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(nextConfig, {
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+})
